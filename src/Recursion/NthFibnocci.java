cedc662fb1753d70f8fa18d3355fb663ce14b648
akamsh <akashmishra30493@gmail.com>
2021-01-04 18:30:02 +0530

    updated fibnocci with recursion and DP

--- src/Recursion/NthFibnocci.java
+++ src/Recursion/NthFibnocci.java
@@ -3,8 +3,11 @@ package Recursion;
 import java.util.Arrays;
 import java.util.HashMap;
 
+//how to find nth fibonacci
 public class NthFibnocci {
     long[] DP = new long[100000];
+
+    //using recursion
     public int returnNthFibnocci(int n) {
         if (n == 0) {
             return -1;
@@ -17,6 +20,8 @@ public class NthFibnocci {
         return x + y;
 
     }
+
+    //using Dynamic programming
     public long returnNthFibnocciDP(int n) {
         if (n <= 0) {
             return -1l;
@@ -24,7 +29,7 @@ public class NthFibnocci {
         if (n == 1 || n == 2) {
             return 1l;
         }
-        if(DP[n] != -1){
+        if (DP[n] != -1) {
             return DP[n];
         }
         DP[n] = returnNthFibnocciDP(n - 1) + returnNthFibnocciDP(n - 2);
@@ -33,10 +38,11 @@ public class NthFibnocci {
     }
 
     public HashMap<Integer, Long> hm = new HashMap<Integer, Long>();
+    //using hashmap
     public Long getFibonacciNumberTopDown(int n) {
-        if(hm.containsKey(n)) return hm.get(n);
-        if(n == 0) return 0L;
-        if(n == 1) return 1L;
+        if (hm.containsKey(n)) return hm.get(n);
+        if (n == 0) return 0L;
+        if (n == 1) return 1L;
         Long nthValue = getFibonacciNumberTopDown(n - 1) + getFibonacciNumberTopDown(n - 2);
         hm.put(n, nthValue);
         return nthValue;
@@ -44,9 +50,10 @@ public class NthFibnocci {
 
     public static void main(String args[]) {
         NthFibnocci nth = new NthFibnocci();
+        //best way to initialize array with -1
         Arrays.fill(nth.DP, -1);
 //        System.out.println(Arrays.toString(nth.DP));
-        System.out.println(nth.returnNthFibnocciDP(50));
+        System.out.println(nth.returnNthFibnocciDP(7));
 //        System.out.println(nth.getFibonacciNumberTopDown(50));
         //System.out.println(nth.returnNthFibnocci(30));
 
