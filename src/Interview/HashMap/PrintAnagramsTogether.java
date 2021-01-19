44d279d32dcd2ccf9d1060db30dcc5ef1b05dc08
akamsh <akashmishra30493@gmail.com>
2021-01-19 14:26:56 +0530

    interview bit question and interview experience

---
+++ src/Interview/HashMap/PrintAnagramsTogether.java
@@ -0,0 +1,42 @@
+package Interview.HashMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+//https://www.geeksforgeeks.org/given-a-sequence-of-words-print-all-anagrams-together/
+public class PrintAnagramsTogether {
+    public String sortString(String s) {
+        char[] c = s.toCharArray();
+        Arrays.sort(c);
+        return new String(c);
+    }
+
+    public void printAnagrams(String input[]) {
+        HashMap<String, ArrayList<String>> storeAnagrams = new HashMap<>();
+        for (int i = 0; i < input.length; i++) {
+            String s = input[i];
+            String sortedString = sortString(input[i]);
+            if (storeAnagrams.containsKey(sortedString)) {
+                storeAnagrams.get(sortedString).add(s);
+            } else {
+                ArrayList<String> current = new ArrayList<>();
+                current.add(s);
+                storeAnagrams.put(sortedString, current);
+            }
+        }
+        for (String s : storeAnagrams.keySet()) {
+            ArrayList<String> values = storeAnagrams.get(s);
+            if (values.size() >=1) {
+                System.out.println(values);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        PrintAnagramsTogether pa=new PrintAnagramsTogether();
+        String input[] = {"cat", "tac","dog", "god","ased","desa","akash","kasha"};
+        pa.printAnagrams(input);
+
+
+    }
+}
---
+++ src/Interview/String/TwoStringsAreAnagrams.java
@@ -0,0 +1,36 @@
+package Interview.String;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoStringsAreAnagrams {
+    public boolean checkAnagrams(String s1, String s2) {
+        HashMap<Character, Integer> countStore = new HashMap<>();
+        for (char i = 0; i < s1.length(); i++) {
+            int count = countStore.containsKey(s1.charAt(i)) ? countStore.get(s1.charAt(i)) : 0;
+            countStore.put(s1.charAt(i), count + 1);
+        }
+        for (char i = 0; i < s2.length(); i++) {
+            if (countStore.containsKey(s2.charAt(i))) {
+                int count = countStore.get(s2.charAt(i));
+                countStore.put(s2.charAt(i), count - 1);
+            }
+        }
+        for (Integer x : countStore.values()) {
+            if (x != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]) {
+        TwoStringsAreAnagrams ts = new TwoStringsAreAnagrams();
+        String s1 = "abc";
+        String s2 = "cataa";
+        String s3 = "tac";
+        System.out.println(ts.checkAnagrams(s2, s3));
+
+    }
+}
