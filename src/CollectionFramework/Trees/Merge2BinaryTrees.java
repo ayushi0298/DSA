f7c78f990bbabc2f97dca58debcff4cb540400fd
akamsh <akashmishra30493@gmail.com>
2021-01-30 18:39:00 +0530

    interview bit question and interview experience

---
+++ src/CollectionFramework/Trees/Merge2BinaryTrees.java
@@ -0,0 +1,76 @@
+package CollectionFramework.Trees;
+
+import com.sun.source.tree.Tree;
+
+//https://www.interviewbit.com/problems/merge-two-binary-tree/
+public class Merge2BinaryTrees {
+    static TreeNode root1;
+    static TreeNode root2;
+
+
+    public class TreeNode {
+        int val;
+        TreeNode right;
+        TreeNode left;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode createTree1() {
+
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        root1 = a;
+        root1.left = b;
+        root1.right = c;
+        return root1;
+
+    }
+
+    public TreeNode createTree12() {
+
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(4);
+        root2 = a;
+        root2.left = b;
+        return root2;
+
+    }
+
+    // public TreeNode mergeTrees(TreeNode A, TreeNode B)
+    public TreeNode solve(TreeNode A, TreeNode B) {
+        if (A == null) {
+            return B;
+        }
+        if (B == null) {
+            return A;
+        }
+        A.val = A.val + B.val;
+        A.left = solve(A.left, B.left);
+        A.right = solve(A.right, B.right);
+        return A;
+
+    }
+
+    public void printTree(TreeNode node) {
+        if (node != null) {
+            System.out.println(node.val);
+            printTree(node.left);
+            printTree(node.right);
+        }
+
+    }
+
+
+    public static void main(String args[]) {
+        Merge2BinaryTrees mb = new Merge2BinaryTrees();
+        root1 = mb.createTree1();
+        root2 = mb.createTree12();
+        TreeNode x = mb.solve(root1, root2);
+        mb.printTree(x);
+
+    }
+}
---
+++ src/Interview/String/LongestCommonPath.java
@@ -0,0 +1,32 @@
+package Interview.String;
+
+import java.util.ArrayList;
+//https://www.geeksforgeeks.org/adobe-interview-experience-off-campus/
+public class LongestCommonPath {
+    public String findCommonPath(ArrayList<String> input) {
+        String temp = input.get(0), common;
+        int size = input.size();
+        for (int i = 1; i < size; i++) {
+            common = "";
+            for (int j = 0; j < Math.min(temp.length(), input.get(i).length()); j++) {
+                if (temp.charAt(j) == input.get((i)).charAt(j)) {
+                    common = common + temp.charAt(j);
+                } else {
+                    break;
+                }
+            }
+            temp = common;
+        }
+        return temp;
+    }
+
+    public static void main(String args[]) {
+        LongestCommonPath ld = new LongestCommonPath();
+        ArrayList<String> input = new ArrayList<>();
+        input.add("abcdes");
+        input.add("abcd");
+        input.add("abc");
+        System.out.println(ld.findCommonPath(input));
+
+    }
+}
