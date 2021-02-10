a930b43fdb97ea6a5cec79c12df7829dd9538d26
akamsh <akashmishra30493@gmail.com>
2021-02-10 19:39:01 +0530

    interview bit question and interview experience

---
+++ src/CollectionFramework/Trees/PathToGivenNode.java
@@ -0,0 +1,64 @@
+package CollectionFramework.Trees;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+//https://www.interviewbit.com/problems/path-to-given-node/
+public class PathToGivenNode {
+
+    public class TreeNode {
+        int val;
+        TreeNode right;
+        TreeNode left;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static TreeNode root;
+
+    public TreeNode createTree1() {
+
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        root = a;
+        root.left = b;
+        root.right = c;
+        return root;
+
+    }
+
+    public boolean findPath(TreeNode root, int B, ArrayList<Integer> path) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == B) {
+            path.add(root.val);
+            return true;
+        }
+        boolean leftFound = findPath(root.left, B, path);
+        boolean rightFound = findPath(root.right, B, path);
+        if (leftFound == true || rightFound == true) {
+            path.add(root.val);
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Integer> solve(TreeNode root, int B) {
+        ArrayList<Integer> result = new ArrayList<>();
+        findPath(root, B, result);
+        Collections.reverse(result);
+        return result;
+    }
+
+    public static void main(String args[]) {
+        PathToGivenNode pg = new PathToGivenNode();
+        root = pg.createTree1();
+        int B = 3;
+        System.out.println(pg.solve(root, B));
+
+    }
+}
