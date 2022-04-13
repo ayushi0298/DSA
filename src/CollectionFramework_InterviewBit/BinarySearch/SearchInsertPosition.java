7c635f74274cfc30d6fe45f2b0cb3e77be999b85
akamsh <ayushimishra121998@gmail.com>
2022-04-13 22:19:35 +0530

    practice

---
+++ src/CollectionFramework_InterviewBit/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,29 @@
+package CollectionFramework_InterviewBit.BinarySearch;
+
+//https://leetcode.com/problems/search-insert-position/
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end + 1;
+    }
+
+    public static void main(String args[]) {
+        SearchInsertPosition si = new SearchInsertPosition();
+        int[] array = new int[]{1, 3, 5, 6};
+        int target = 2;
+        System.out.println(si.searchInsert(array, target));
+
+    }
+
+}
